61bbacb1ed6fd8d33e64facc4a1cb810e6e147e0
Zuzanna Szymczak <zuzannaolgaszymczak@gmail.com>
2023-01-22 20:15:43 +0000

    Rails 2 - 2- checkbox checked

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,8 +1,13 @@
 module TasksHelper
 
-  def index(tasks)
-    tasks.each do |task|
-      return task
-    end
+  # def index(tasks)
+  #   tasks.each do |task|
+  #     current = task
+  #   end
+  #     if current.completed == true
+  #      return 'checked'
+  #     else
+  #      return ''
+  #     end
+  #   end
   end
-end
